c0b4ebbe9984986aedf22d20a1455ed188a2c1b6
Harlow7777 <Harlow_Jacob@outlook.com>
2023-05-01 22:57:15 -0500

    adding the rest of the creatures

--- js/navigation.js
+++ js/navigation.js
@@ -25,21 +25,87 @@ function showDiv(linkId, divId) {
 	link.style.color = 'gray';
 }
 
-function showOnly(parent, className) {
-	className = ' ' + className + ' ';
+var activeFilters = []
 
-	var img = document.getElementById(className.trim() + "-filter-button-img");
-	img.setAttribute("style","-webkit-filter:brightness(70%)");
+function showOnly(imgName, className) {
+	var img = document.getElementById(imgName);
+	var noFilterImg = document.getElementById('no-filter-button-img');
 
-	var e = parent.firstChild;
-	while (e != null) {
-		if (e.nodeType == 1) {
-		if ((' ' + e.className + ' ').indexOf(className) > -1)
-			e.style.display = 'flex';
-		else
-			e.style.display = 'none';
+	// if off, turn it on
+	if (img.classList.contains('off')) {
+		activeFilters.push(className);
+		if (noFilterImg.classList.contains('on')) {
+			while (activeFilters.indexOf('card') > -1)
+				delete activeFilters[activeFilters.indexOf('card')];
+			noFilterImg.classList.remove('on');
+			noFilterImg.classList.add('off');
 		}
 
-		e = e.nextSibling;
+		// if no-filter, disable all active filter buttons
+		if (imgName == 'no-filter-button-img') {
+			activeFilters = []
+			var filterDivs = []
+			filterDivs.push(document.getElementById('filter'));
+			filterDivs.push(document.getElementById('type-filter'));
+			filterDivs.forEach(filterDiv=> {
+				var e = filterDiv.firstChild;
+				while (e != null) {
+					if (e.nodeType == 1) {
+						if (e.hasChildNodes) {
+							filterImg = e.firstChild.nextSibling;
+							if (filterImg.classList != undefined && filterImg.classList.contains("on")) {
+								filterImg.classList.remove("on");
+								filterImg.classList.add("off");
+							}
+						}
+					}
+					e = e.nextSibling;
+				}
+			});
+		}
+
+		img.classList.remove('off');
+		img.classList.add('on');
+
+		const parentDivs = [];
+		parentDivs.push(document.getElementById('creatures')); 
+		parentDivs.push(document.getElementById('spells')); 
+		parentDivs.push(document.getElementById('locations')); 
+		parentDivs.forEach(parent => {
+			var e = parent.firstChild.nextSibling;
+			while (e != null) {
+				if (e.nodeType == 1) {
+					if (imgName == 'no-filter-button-img' || activeFilters.some(r=> e.classList.contains(r)))
+						e.style.display = 'flex';
+					else
+						e.style.display = 'none';
+				}
+
+				e = e.nextSibling;
+			}
+		});
+	}
+	// if on, turn it off
+	else if (img.classList.contains('on')) {
+		while (activeFilters.indexOf(className) > -1)
+			delete activeFilters[activeFilters.indexOf(className)];
+		img.classList.remove('on');
+		img.classList.add('off');
+
+		const parentDivs = [];
+		parentDivs.push(document.getElementById('creatures')); 
+		parentDivs.push(document.getElementById('spells')); 
+		parentDivs.push(document.getElementById('locations')); 
+		parentDivs.forEach(parent => {
+			var e = parent.firstChild.nextSibling;
+			while (e != null) {
+				if (e.nodeType == 1) {
+					if (!activeFilters.some(r=> e.classList.contains(r)))
+						e.style.display = 'none';
+				}
+
+				e = e.nextSibling;
+			}
+		});
 	}
 }
